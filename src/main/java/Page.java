5310ad9e430c44f273e1e68e3cf075c632be2e49
kirill <kirill.deshkev@gmail.com>
2019-12-08 23:12:56 +0300

    Improve locators

--- src/main/java/Page.java
+++ src/main/java/Page.java
@@ -1,7 +1,13 @@
-import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.$$x;
+
 
 public class Page {
+    public void getHeadersRow(){
+        var headerElements = $$("knekwnf");
+    }
+
     public void getUSDRow() {
-        var USDRowElements = $x("//table[@class=\"b-currency-table__best\" and contains(. , \"1 USD\")]//td ");
+        var USDRowElements = $$x("//table[@class=\"b-currency-table__best\" and contains(. , \"1 USD\")]//td ");
     }
 }
