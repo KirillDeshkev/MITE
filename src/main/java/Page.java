4943029f916f56575218a3395fe3261c9978dd8c
KirillDeshkev <kirill.deshkev@gmail.com>
2019-12-10 17:23:15 +0300

    Add locator for header

--- src/main/java/Page.java
+++ src/main/java/Page.java
@@ -1,13 +1,29 @@
-import static com.codeborne.selenide.Selenide.$$;
+import com.codeborne.selenide.ElementsCollection;
+
 import static com.codeborne.selenide.Selenide.$$x;
 
 
 public class Page {
-    public void getHeadersRow(){
-        var headerElements = $$("knekwnf");
+    public ElementsCollection getHeadersRow() {
+        return  $$x("//table[1]/thead/tr/th");
     }
 
     public void getUSDRow() {
-        var USDRowElements = $$x("//table[@class=\"b-currency-table__best\" and contains(. , \"1 USD\")]//td ");
+        var USDRowElements = $$x("//table[@class=\"b-currency-table__best\" and contains(. , \"1 USD\")]//td");
+    }
+
+    public void getEURRow() {
+        var USDRowElements = $$x("//table[@class=\"b-currency-table__best\" and contains(. , \"1 EUR\")]//td");
+    }
+
+    public void getBuyRate(){
+        var tableHeaders = getHeadersRow();
+        for (int i = 0; i < tableHeaders.size(); i++) {
+            if (tableHeaders.get(i).getText().equals("Банк покупает")){
+                int buyColumn = i;
+            }
+        }
+
+        var
     }
 }
---
+++ src/main/java/Steps.java
@@ -0,0 +1,11 @@
+public class Steps {
+    public void getBuyRate(){
+        var tableHeaders = new Page();
+
+    }
+    public void getSellRate(){
+
+    }
+
+    public void get
+}
